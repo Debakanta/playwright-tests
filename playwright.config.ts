3933697de07502e8f35aa3145a65a5ce5df767a4
Debakanta <debakantasahu13@gmail.com>
2026-09-07 19:24:06 +0530

    Updating playwright config for headless:true

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   reporter: [['list'], ['html'], ['allure-playwright']],
   use: {
     baseURL: 'https://www.saucedemo.com',
-    headless: false,
+    headless: true,
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
